9a46c0f575e6ae66707cb1e22f1e88dbfe8c8ed5
Ben Buckton <contact@benbuckton.com>
2020-12-03 15:31:01 +0100

    first big push

---
+++ Assets/Scripts/RTSCommand.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class RTSCommand : MonoBehaviour {
+    public Camera cam;
+    public NavMeshSurface navMesh;
+    public int mouseButtonIndex = 0;
+    private bool _instantTravel = false;
+    private ThirdPersonCharacter _character;
+    public NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null) {
+            _instantTravel = true;
+        }
+
+        _character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(mouseButtonIndex)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (_instantTravel) {
+                    transform.SetPositionAndRotation(hit.point,Quaternion.identity);   
+                }
+                else {
+                    agent.SetDestination(hit.point);
+                }
+            }
+
+            if (agent.remainingDistance > agent.stoppingDistance) {
+                _character.Move(agent.desiredVelocity,false,false);    
+            }
+            else {
+                _character.Move(Vector3.zero, false,false);
+            }
+            
+            navMesh.BuildNavMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/StayAtMidpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayAtMidpoint : MonoBehaviour
+{
+    public Transform firstPoint;
+    public Transform secondPoint;
+    private Vector3 _lastPosition;
+
+    void Start() {
+        _lastPosition = transform.position;
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        var direction = _lastPosition + transform.position;
+        transform.SetPositionAndRotation(firstPoint.position + (secondPoint.position - firstPoint.position) /2, Quaternion.Euler(direction));    
+    }
+}
