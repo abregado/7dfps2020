492c910318732e828283f7cd56b4b3734ba754d9
Tom Mason <wheybags@wheybags.com>
2020-12-05 14:02:06 +0000

    Added weapon viewmodel

---
+++ Assets/Scripts/WarriorCharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorCharacterController : FirstPersonController
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
